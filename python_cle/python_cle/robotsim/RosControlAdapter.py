74e6c9266d1cfd52c3e340349c40ed3d7c49b571
Nino Cauli <nigno@nigno-laptop.(none)>
2014-09-18 09:21:26 +0100

    added a variable time_step in order to not read the physics time step each time the run is called
    Change-Id: I4cb3b2b671c4d9116d1fa26f7b80573e14b01ab6

--- python_cle/python_cle/robotsim/RosControlAdapter.py
+++ python_cle/python_cle/robotsim/RosControlAdapter.py
@@ -11,14 +11,11 @@ __author__ = 'NinoCauli'
 class RosControlAdapter(IRobotControlAdapter):
     def initialize(self):
         rospy.wait_for_service('/gazebo/get_physics_properties')
-        self.__get_physics_properties = rospy.ServiceProxy('gazebo/get_physics_properties',
-                                                           GetPhysicsProperties)
+        self.__get_physics_properties = rospy.ServiceProxy('gazebo/get_physics_properties', GetPhysicsProperties)
         rospy.wait_for_service('/gazebo/get_world_properties')
-        self.__get_world_properties = rospy.ServiceProxy('gazebo/get_world_properties',
-                                                         GetWorldProperties)
+        self.__get_world_properties = rospy.ServiceProxy('gazebo/get_world_properties', GetWorldProperties)
         rospy.wait_for_service('/gazebo/set_physics_properties')
-        self.__set_physics_properties = rospy.ServiceProxy('gazebo/set_physics_properties',
-                                                           SetPhysicsProperties)
+        self.__set_physics_properties = rospy.ServiceProxy('gazebo/set_physics_properties', SetPhysicsProperties)
         rospy.wait_for_service('/gazebo/pause_physics')
         self.__pause_client = rospy.ServiceProxy('gazebo/pause_physics', Empty)
         rospy.wait_for_service('/gazebo/reset_sim')
@@ -26,24 +23,25 @@ class RosControlAdapter(IRobotControlAdapter):
         rospy.wait_for_service('gazebo/end_world')
         self.__endWorld = rospy.ServiceProxy('gazebo/end_world', Empty)
         rospy.wait_for_service('gazebo/advance_simulation')
-        self.__advance_simulation = rospy.ServiceProxy('gazebo/advance_simulation',
-                                                       AdvanceSimulation)
+        self.__advance_simulation = rospy.ServiceProxy('gazebo/advance_simulation', AdvanceSimulation)
         physics = self.__get_physics_properties()
         paused = physics.pause
         if (not paused):
             self.__pause_client()
         self.__reset()
+        self.get_time_step()
         pass
 
     def get_time_step(self):
         physics = self.__get_physics_properties()
-        time_step = physics.time_step
-        return time_step
+        self.__time_step = physics.time_step
+        return self.__time_step
 
     def set_time_step(self, time_step):
         physics = self.__get_physics_properties()
-        success = self.__set_physics_properties(time_step, physics.max_update_rate, physics.gravity,
-                                                physics.ode_config)
+        success = self.__set_physics_properties(time_step, physics.max_update_rate, physics.gravity, physics.ode_config)
+        if (success):
+            self.__time_step = time_step
         return success
 
     def is_paused(self):
@@ -57,9 +55,8 @@ class RosControlAdapter(IRobotControlAdapter):
         return success
 
     def run_step(self, dt):
-        physics = self.__get_physics_properties()
-        if dt % physics.time_step == 0:
-            steps = dt / physics.time_step
+        if dt % self.__time_step == 0:
+            steps = dt / self.__time_step
             self.__advance_simulation(steps)
             world = self.__get_world_properties()
             simTime = world.sim_time
@@ -69,6 +66,5 @@ class RosControlAdapter(IRobotControlAdapter):
         return simTime
 
     def shutdown(self):
-        # TODO test better the endWorld function recently added in the gazebo_ros_HBP plugin
-        # self.__endWorld()
+        self.__endWorld()
         pass
