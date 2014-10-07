e092bb44b12bb86000b63645906b438772ef5b3b
Lars Pfotzer <pfotzer@fzi.de>
2014-10-07 08:15:53 +0200

    Improved run_tests.sh for running the tests without separately starting software (e.g. roscore, gazebo, ...).
    Change-Id: I096a818b6290851aae399e496856738a58e97c52

--- run_tests.sh
+++ run_tests.sh
@@ -1,1 +1,27 @@
+#!/bin/bash
+
+# Installing the ros package to control gazebo
+cd GazeboRosPackage/src/
+catkin_init_workspace
+cd ..
+catkin_make
+source devel/setup.bash
+# catkin_make install
+cd ..
+
+# start roscore to enable tests using ROS
+echo "Starting roscore"
+roscore &
+echo "Waiting until roscore is started"
+until rostopic list ; do sleep 1; done
+
+echo "Starting gazebo"
+rosrun gazebo_ros gzserver &
+sleep 5
+
 make test
+
+# Cleanup after ourselves
+pgrep -f ros | xargs kill -9
+killall gzserver
+
