426c786469329a459bf30b64cc9ef5545273662d
cauli <cauli@bbpcode.epfl.ch>
2014-10-22 17:11:49 +0100

    reactivating test_robot_controller 3
    Change-Id: I65481b2643db7d354c7fcaf10bbf80c814220330

--- python_cle/python_cle/tests/robotsim/test_robot_control_adapter.py
+++ python_cle/python_cle/tests/robotsim/test_robot_control_adapter.py
@@ -5,27 +5,27 @@ import unittest
 __author__ = 'Lorenzo Vannucci'
 
 
-# class TestRosControlAdapter(unittest.TestCase):
-#
-#     def setUp(self):
-#         self._rca = RosControlAdapter()
-#         self._rca.initialize()
-#
-#     def test_time_step(self):
-#         self.assertTrue(self._rca.set_time_step(0.01))
-#         self.assertEqual(self._rca.time_step, 0.01)
-#
-#     def test_is_paused(self):
-#         self.assertTrue(self._rca.is_paused)
-#
-#     def test_is_alive(self):
-#         self.assertTrue(self._rca.is_alive)
-#
-#     def test_run_step(self):
-#         self.assertEqual(self._rca.run_step(0.05), 0.05)
-#         with self.assertRaises(ValueError):
-#             self._rca.run_step(0.0001)
-#
+class TestRosControlAdapter(unittest.TestCase):
+
+    def setUp(self):
+        self._rca = RosControlAdapter()
+        self._rca.initialize()
+
+    def test_time_step(self):
+        self.assertTrue(self._rca.set_time_step(0.01))
+        self.assertEqual(self._rca.time_step, 0.01)
+
+    def test_is_paused(self):
+        self.assertTrue(self._rca.is_paused)
+
+    def test_is_alive(self):
+        self.assertTrue(self._rca.is_alive)
+
+    def test_run_step(self):
+        self.assertEqual(self._rca.run_step(0.05), 0.05)
+        with self.assertRaises(ValueError):
+            self._rca.run_step(0.0001)
+
 #     def test_shutdown(self):
 #         self._rca.shutdown()
 
