df0ac287289984dc583550395f76cd6f3bc99d2d
Lars Pfotzer <pfotzer@fzi.de>
2015-01-14 23:07:02 +0100

    Changed colour detection based on channels to pixel-wise red detection.
    Change-Id: Idbd19e2b52be974b5aed49614acb602b3bfa2687

--- hbp_nrp_cle/hbp_nrp_cle/tf_framework/tf_lib.py
+++ hbp_nrp_cle/hbp_nrp_cle/tf_framework/tf_lib.py
@@ -23,17 +23,40 @@ def detect_red(image):
         # print eye_sensor.changed
         cv_image = bridge.imgmsg_to_cv2(image, "rgb8") / 256.
         cv_image = 5000 ** cv_image / 5000
+
         for i in range(0, cv_image.shape[0]):
             for j in range(0, cv_image.shape[1]):
-                if j < cv_image.shape[1] / 2:
-                    red_left_rate += cv_image[i, j, 0]
+                r = cv_image[i, j, 0]
+                g = cv_image[i, j, 1]
+                b = cv_image[i, j, 2]
+
+                if r > 2 * (g + b):
+                    if j < cv_image.shape[1] / 2:
+                        red_left_rate += 1.
+                    else:
+                        red_right_rate += 1.
                 else:
-                    red_right_rate += cv_image[i, j, 0]
-                green_blue_rate += cv_image[i, j, 1]
-                green_blue_rate += cv_image[i, j, 2]
-        red_left_rate *= (1. / cv_image.size)
-        red_right_rate *= (1. / cv_image.size)
-        green_blue_rate *= (1. / cv_image.size)
+                    green_blue_rate += 1.
+
+        red_left_rate *= (6. / cv_image.size)
+        red_right_rate *= (6. / cv_image.size)
+        green_blue_rate *= (3. / cv_image.size)
+
+        print "red_left_rate: ", red_left_rate
+        print "red_right_rate: ", red_right_rate
+        print "green_blue_rate: ", green_blue_rate
+
+#        for i in range(0, cv_image.shape[0]):
+#            for j in range(0, cv_image.shape[1]):
+#                if j < cv_image.shape[1] / 2:
+#                    red_left_rate += cv_image[i, j, 0]
+#                else:
+#                    red_right_rate += cv_image[i, j, 0]
+#                green_blue_rate += cv_image[i, j, 1]
+#                green_blue_rate += cv_image[i, j, 2]
+#        red_left_rate *= (1. / cv_image.size)
+#        red_right_rate *= (1. / cv_image.size)
+#        green_blue_rate *= (1. / cv_image.size)
 
     class __results(object):
         """
