fcf6bfa01a6d93a85b54eb3f8df15de495a8374f
Daniel Peppicelli <daniel.peppicelli@epfl.ch>
2015-02-24 11:55:01 +0100

    Refactoring of ROSCLEClient Added state service on the server side
    Change-Id: I63ae5756f1e7761974db85859ddf1760d370f04f

--- hbp_nrp_cle/hbp_nrp_cle/cle/ROSCLEClient.py
+++ hbp_nrp_cle/hbp_nrp_cle/cle/ROSCLEClient.py
@@ -1,52 +1,145 @@
 """
-Helper class that takes care of making the appropriate ROS call(s) to
-control a simulation.
+Takes care of making the appropriate ROS call(s) to control a simulation.
+On the other side of ROS, the calls are handled by ROSCLEServer.py
 """
 
+import logging
 import rospy
 from std_srvs.srv import Empty
+# This package comes from the catkin package ROSCLEServicesDefinitions
+# in the GazeboRosPackage folder at the root of the CLE (this) repository.
+from ROSCLEServicesDefinitions import srv
+from hbp_nrp_cle.cle.ROSCLEState import ROSCLEState
+
+__author__ = "Lorenzo Vannucci, Daniel Peppicelli"
+logger = logging.getLogger(__name__)
 
 
 class ROSCLEClient(object):
     """
-    A ROS server wrapper around the Closed Loop Engine.
+    Client around the ROS controlled Closed Loop Engine.
     """
+
+    ROS_SERVICE_TIMEOUT = 120
+    ROS_CLE_NODE_NAME = "ros_cle_simulation"
+    ROS_CLE_URI_PREFIX = "/" + ROS_CLE_NODE_NAME
 
     def __init__(self):
         """
         Create the wrapper client
         """
 
-        # create service proxies
-        self.cle_start = rospy.ServiceProxy('/ros_cle_simulation/start', Empty)
-        self.cle_start.wait_for_service()
-        self.cle_pause = rospy.ServiceProxy('/ros_cle_simulation/pause', Empty)
-        self.cle_pause.wait_for_service()
-        self.cle_stop = rospy.ServiceProxy('/ros_cle_simulation/stop', Empty)
-        self.cle_stop.wait_for_service()
-        self.cle_reset = rospy.ServiceProxy('/ros_cle_simulation/reset', Empty)
-        self.cle_reset.wait_for_service()
+        self.__valid = True
+        self.__invalid_reason = ""
+
+        # Creates service proxies
+        self.__cle_start = self.__init_ros_service(self.ROS_CLE_URI_PREFIX + '/start', Empty)
+        self.__cle_pause = self.__init_ros_service(self.ROS_CLE_URI_PREFIX + '/pause', Empty)
+        self.__cle_stop = self.__init_ros_service(self.ROS_CLE_URI_PREFIX + '/stop', Empty)
+        self.__cle_reset = self.__init_ros_service(self.ROS_CLE_URI_PREFIX + '/reset', Empty)
+        self.__cle_state = self.__init_ros_service(self.ROS_CLE_URI_PREFIX + '/state',
+                                                   srv.get_simulation_state)
+
+    def __init_ros_service(self, service_name, service_class):
+        """
+        Initialize a ROS service proxy
+
+        @param: service_name: The name of the service
+        @param: service_class: The class (.srv) of the service. This class
+                               is generated from the ROS catckin package .srv files.
+        """
+        handler = None
+        if self.__valid:
+            try:
+                logger.info("Connecting to ROS service " + service_name)
+                handler = rospy.ServiceProxy(service_name, service_class)
+                handler.wait_for_service(timeout=self.ROS_SERVICE_TIMEOUT)
+            except rospy.ROSException:
+                # According to the documentation, only a timeout will raise a generic
+                # 'ROSException'.
+                # http://docs.ros.org/api/rospy/html/rospy-module.html#wait_for_service
+                error = "Timeout while connecting to the CLE (waiting on " + service_name + ")."
+                logger.error(error)
+                raise ROSCLEClientException(error)
+        return handler
+
+    def __call_service(self, service):
+        """
+        Generic call handler for all the ROS service proxies that are based on the Empty class.
+        The call handler will mask excpetions from ROS and will discard the client if there is
+        something wrong going on.
+
+        @param: handler: The ROS proxy service to call
+        """
+        if self.__valid:
+            try:
+                service()
+            except rospy.ServiceException:
+                self.__valid = False
+                self.__invalid_reason = "a previous communication error"
+                error_message = "Impossible to communicate with the CLE, discarding the client."
+                raise ROSCLEClientException(error_message)
+        else:
+            raise ROSCLEClientException("Client has been discarded due to " +
+                                        self.__invalid_reason +
+                                        ".")
 
     def start(self):
         """
         Start the simulation.
         """
-        self.cle_start()
+        self.__call_service(self.__cle_start)
 
     def pause(self):
         """
         Pause the simulation.
         """
-        self.cle_pause()
+        self.__call_service(self.__cle_pause)
 
     def stop(self):
         """
         Stop the simulation.
         """
-        self.cle_stop()
+        self.__call_service(self.__cle_stop)
+        # Once the stop handler has been called, we do not expect it to answer anymore
+        self.__valid = False
+        self.__invalid_reason = "a previous stop request (triggering automatic disconnection)"
 
     def reset(self):
         """
         Reset the simulation.
         """
-        self.cle_reset()
+        self.__call_service(self.__cle_reset)
+
+    def get_simulation_state(self):
+        """
+        Get the simulation state.
+        """
+        # By default, we assume an experiment is in the stop state
+        # (whether it is really stop or if something bad did happen.)
+        state = ROSCLEState.STOPPED
+        if (self.__valid):
+            try:
+                state = str(self.__cle_state().state)
+            except rospy.ServiceException as e:
+                logger.error("Error while trying to retrieve simulation state: " +
+                             e.message +
+                             ". Returning stopped as state.")
+        else:
+            logger.warn("Trying to retrieve the state of a simulation from an invalid client " +
+                        "(invalid due to " +
+                        self.__invalid_reason +
+                        ") ")
+        return state
+
+
+class ROSCLEClientException(Exception):
+    """
+    Exception within the CLE client
+    """
+    def __init__(self, value):
+        super(ROSCLEClientException, self).__init__()
+        self.parameter = value
+
+    def __str__(self):
+        return repr(self.parameter)
