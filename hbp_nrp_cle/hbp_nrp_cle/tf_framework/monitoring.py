11ae896eb9b1a2b93d896d70e8310b76f1a1f455
cauli <cauli@bbpcode.epfl.ch>
2015-03-26 21:52:27 +0000

    Changes to the backend spike monitoring in order to publish the spikes at each tick in a format similar to the frontend
    Change-Id: I94967f3d9c8fe063e543548953bc6851b2abca01

---
+++ hbp_nrp_cle/hbp_nrp_cle/tf_framework/monitoring.py
@@ -0,0 +1,25 @@
+"""
+This module contains functions supporting monitoring of neurons
+"""
+
+__author__ = 'GeorgHinkel'
+
+from cle_ros_msgs.msg import SpikeEvent, SpikeData
+
+
+def create_spike_recorder_message(t, neuronCount, spikes, monitor_name):
+    """
+    Creates a monitoring message for the given simulation time and spike counts
+    :param t: The simulation time
+    :param neuronCount: The amount of neurons monitored in total
+    :param spikes: The spikes received
+    :param monitor_name: The name of the monitor
+    :return:
+    """
+    if spikes is None or spikes.shape[1] == 0:
+        return None
+
+    msgs = []
+    for spike in spikes:
+        msgs.append(SpikeData(int(spike[0]), spike[1]))
+    return SpikeEvent(t, neuronCount, msgs, monitor_name)
