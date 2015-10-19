2c290fa936330385de1342ed0d6693dd968d5edd
Georg Hinkel <hinkel@fzi.de>
2015-10-19 18:13:14 +0200

    [NO STORY] Small test to raise code coverage. Coverage of detect_red and get_color_values is not tackled so far.
    Change-Id: I59f67904149ad979e85746934591dbc7f249fb42

--- hbp_nrp_cle/hbp_nrp_cle/tests/tf_framework/test_tf_lib.py
+++ hbp_nrp_cle/hbp_nrp_cle/tests/tf_framework/test_tf_lib.py
@@ -29,7 +29,13 @@ class TestTFLib(unittest.TestCase):
     def test_camera(self):
         w, h = 640, 480
         c = tf_lib.Camera()
-        c.set_image_size(w, h)
+
+        self.assertFalse(c.set_image_size(-w, h))
+        self.assertFalse(c.set_image_size(w, -h))
+        self.assertTrue(c.set_image_size(w, h))
+
+        self.assertEqual(w, c.width)
+        self.assertEqual(h, c.height)
 
         for i in range(0, 1000):
             rand_px = (
@@ -73,6 +79,72 @@ class TestTFLib(unittest.TestCase):
             self.assertLessEqual(abs(n2p[1] - rand_px[1]), 0.1)
             self.assertLessEqual(abs(a2p[0] - rand_px[0]), 0.1)
             self.assertLessEqual(abs(a2p[1] - rand_px[1]), 0.1)
+
+    def test_angle_pixel(self):
+        c = tf_lib.Camera()
+        c.set_image_size(20, 20)
+        self.__test_angle_pixel(c, 20, 20)
+        self.__test_angle_pixel(c, 0, 0)
+        self.__test_angle_pixel(c, 0, 20)
+        self.__test_angle_pixel(c, 10, 5)
+
+    def __test_angle_pixel(self, c, x, y):
+        u, v = c.pixel2angle(x, y)
+        _x, _y = c.angle2pixel(u, v)
+
+        self.assertAlmostEqual(_x, x)
+        self.assertAlmostEqual(_y, y)
+
+    def test_norm_pixel(self):
+        c = tf_lib.Camera()
+        c.set_image_size(20, 20)
+        self.__test_norm_pixel(c, 20, 20)
+        self.__test_norm_pixel(c, 0, 0)
+        self.__test_norm_pixel(c, 0, 20)
+        self.__test_norm_pixel(c, 10, 5)
+
+    def __test_norm_pixel(self, c, x, y):
+        u, v = c.pixel2norm(x, y)
+        _x, _y = c.norm2pixel(u, v)
+
+        self.assertAlmostEqual(_x, x)
+        self.assertAlmostEqual(_y, y)
+
+    def test_norm_angle(self):
+        c = tf_lib.Camera()
+        c.set_image_size(20, 20)
+        self.__test_norm_angle(c, 1, 1)
+        self.__test_norm_angle(c, 0, 0)
+        self.__test_norm_angle(c, -1, 1)
+        self.__test_norm_angle(c, 0.5, -1)
+
+    def __test_norm_angle(self, c, x, y):
+        u, v = c.norm2angle(x, y)
+        t1, t2 = c.angle2pixel(u, v)
+        _x, _y = c.pixel2norm(t1, t2)
+
+        self.assertAlmostEqual(_x, x)
+        self.assertAlmostEqual(_y, y)
+
+    def test_norm_metric(self):
+        c = tf_lib.Camera()
+        c.set_image_size(20, 20)
+        self.__test_norm_metric(c, 1, 1)
+        self.__test_norm_metric(c, 0, 0)
+        self.__test_norm_metric(c, -1, 1)
+        self.__test_norm_metric(c, 0.5, -1)
+
+    def __test_norm_metric(self, c, x, y):
+        u, v = c.norm2metric(x, y)
+        t1, t2 = c.metric2pixel(u, v)
+        _x, _y = c.pixel2norm(t1, t2)
+
+        self.assertAlmostEqual(_x, x)
+        self.assertAlmostEqual(_y, y)
+
+    def test_exp(self):
+        self.assertAlmostEqual(1, tf_lib.exp_wrapper(0))
+        self.assertAlmostEqual(1, tf_lib.exp_wrapper(1) * tf_lib.exp_wrapper(-1))
 
 
 if __name__ == '__main__':
