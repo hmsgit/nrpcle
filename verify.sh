4a62e1739279703993815b625705b2a23985a551
Kenny Sharma <kenny.sharma@tum.de>
2016-10-28 13:18:22 +0200

    [hotfix] Add .eggs to .gitignore and pylint ignore.
    Change-Id: I1aa9cec208274848e7450eac04e7581631dff4b6

--- verify.sh
+++ verify.sh
@@ -4,7 +4,7 @@
 # Copy open-cv binary
 ./ubuntu_fix_cv2.sh
 
-export IGNORE_LINT="platform_venv|build|hbp_nrp_cle/hbp_nrp_cle/bibi_config/generated|demo/"
+export IGNORE_LINT="platform_venv|build|hbp_nrp_cle/hbp_nrp_cle/bibi_config/generated|demo/|hbp_nrp_cle/.eggs"
 
 # This script only runs static code analysis, the tests can be run separately using run_tests.sh
 make run_pep8 run_pylint
