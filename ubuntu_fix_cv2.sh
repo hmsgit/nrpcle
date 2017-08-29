46edb252041fef33b8342a0a0542ec3c66697283
Axel von Arnim <axel.vonarnim@fortiss.org>
2017-08-29 17:11:58 +0200

    [HOTFIX] Ubuntu_fix script was still copying to old relative platform_venv
    Change-Id: I38f1d1003f4c0c71c7a79a2084282c4c0342ac67

--- ubuntu_fix_cv2.sh
+++ ubuntu_fix_cv2.sh
@@ -2,5 +2,5 @@
 # This imports cv2 to the platform_venv on ubuntu. Use this script on local installations only.
 # You should have installed pyton-opencv via apt-get
 
-mkdir platform_venv/lib/python2.7/site-packages/ -p
-cp /usr/lib/python2.7/dist-packages/cv* platform_venv/lib/python2.7/site-packages/
+mkdir $VIRTUAL_ENV/lib/python2.7/site-packages/ -p
+cp /usr/lib/python2.7/dist-packages/cv* $VIRTUAL_ENV/lib/python2.7/site-packages/
