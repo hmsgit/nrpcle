e128ee5d5b6d5c264d4530b03f6187ccc9275e33
Kenny Sharma <kenny.sharma@tum.de>
2017-09-07 10:10:13 +0200

    [NRRPLT-5584] Make cv2 fix script more specific.
    This patch makes the cv2 script more specific to avoid issues if
    the local installation has other pip packages starting with cv
    installed.
    
    The commit is mirrored in the corresponding user-scripts function.
    
    Change-Id: Id17aad7fb636cd6d915f536665b2e63edc302cbe

--- ubuntu_fix_cv2.sh
+++ ubuntu_fix_cv2.sh
@@ -3,4 +3,5 @@
 # You should have installed pyton-opencv via apt-get
 
 mkdir $VIRTUAL_ENV/lib/python2.7/site-packages/ -p
-cp /usr/lib/python2.7/dist-packages/cv* $VIRTUAL_ENV/lib/python2.7/site-packages/
+cp /usr/lib/python2.7/dist-packages/cv2*.so $VIRTUAL_ENV/lib/python2.7/site-packages/
+cp /usr/lib/python2.7/dist-packages/cv.py $VIRTUAL_ENV/lib/python2.7/site-packages/
