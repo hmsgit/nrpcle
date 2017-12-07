d91452c6007092ab7880a66dfaf0f8a50402ad91
Georg Hinkel <hinkel@fzi.de>
2017-12-07 14:59:12 +0100

    [No Story] Correct handling of VIRTUAL_ENV variable
    Change-Id: I7fe21bd6605e33d800a5bfaf9bd7704991d000c3

--- verify.sh
+++ verify.sh
@@ -5,6 +5,7 @@
 ./ubuntu_fix_cv2.sh
 
 export IGNORE_LINT="platform_venv|build|hbp_nrp_cle/hbp_nrp_cle/bibi_config/generated|demo/|hbp_nrp_cle/.eggs"
+export VIRTUAL_ENV=$NRP_VIRTUAL_ENV
 
 # This script only runs static code analysis, the tests can be run separately using run_tests.sh
 make run_pep8 run_pylint
