c6bd5eaf7831570b0cc9c321fac3c87c50f7a69e
Luc Guyot <luc.guyot.ge@gmail.com>
2018-08-28 12:37:28 +0000

    Merged in gerrit-41075-remove-ubuntu-fix-cv2 (pull request #2)
    [NRRPLT-0000] GERRIT-41075 Removes obsolete file: ubuntu_fix_cv2.sh (the fix is applied by user-scritps)
    
    Approved-by: Axel von Arnim <axel.vonarnim@fortiss.org>

--- ubuntu_fix_cv2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# This imports cv2 to the platform_venv on ubuntu. Use this script on local installations only.
-# You should have installed pyton-opencv via apt-get
-
-mkdir $VIRTUAL_ENV/lib/python2.7/site-packages/ -p
-cp /usr/lib/python2.7/dist-packages/cv2*.so $VIRTUAL_ENV/lib/python2.7/site-packages/
-cp /usr/lib/python2.7/dist-packages/cv.py $VIRTUAL_ENV/lib/python2.7/site-packages/
